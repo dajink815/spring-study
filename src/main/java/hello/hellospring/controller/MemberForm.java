23d799cc8128c5372e9cb8d560188e6ab6e206d9
김다진 <dajink815@uangel.com>
2022-11-17 09:16:50 +0900

    1. 회원관리예제 (웹MVC) - 회원 등록

---
+++ src/main/java/hello/hellospring/controller/MemberForm.java
@@ -0,0 +1,17 @@
+package hello.hellospring.controller;
+
+/**
+ * @author dajin kim
+ */
+public class MemberForm {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
